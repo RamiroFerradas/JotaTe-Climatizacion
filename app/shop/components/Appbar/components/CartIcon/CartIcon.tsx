868bf7b89bb7e88529c4592da0aa16e39a7b5655
RamiroFerradas <ramiferra97@gmail.com>
2023-06-05 22:14:33 -0300

    cart identifer fix

--- app/shop/components/Appbar/components/CartIcon/CartIcon.tsx
+++ app/shop/components/Appbar/components/CartIcon/CartIcon.tsx
@@ -1,5 +1,6 @@
 import { closeMenuCart, openMenuCart } from "@/app/redux/slices/cart";
 import { AppStore } from "@/app/redux/store";
+import { Chip, ListItemSuffix } from "@material-tailwind/react";
 import { Dispatch, SetStateAction } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { useDispatch } from "react-redux";
@@ -27,14 +28,23 @@ const CartIcon: React.FC<CartIconProps> = ({ setopenSidebar }) => {
       <button
         className={`flex-col flex justify-center hover:text-[#006d54] transition-all items-center`}
       >
-        <FaShoppingCart />
+        <ListItemSuffix>
+          <Chip
+            value={cart.length}
+            size="sm"
+            // variant="text"
+            color="red"
+            className="rounded-full absolute"
+          />
+        </ListItemSuffix>
+        <FaShoppingCart className="text-xl" />
 
         <p>Carrito</p>
       </button>
 
-      <span className="absolute top-0 right-1 bg-red-600 text-white font-bold text-xs rounded-full px-1 py-0.3">
+      {/* <span className="absolute top-0 right-1 bg-red-600 text-white font-bold text-xs rounded-full px-[1px]">
         {cart.length}
-      </span>
+      </span> */}
     </div>
   );
 };
