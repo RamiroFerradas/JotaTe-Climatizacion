ab6057a50f2222efa7f1ad96153c737362fa4147
RamiroFerradas <ramiferra97@gmail.com>
2023-06-08 02:38:41 -0300

    feature  function add consults product

--- app/api/mercadopago/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { createPreference } from "./mercadopago";
-
-export async function GET(request: Request) {
-  const preferenceId = await createPreference();
-
-  return NextResponse.json(preferenceId, { status: 200 });
-}
