1d2a024cbb907038ccd96cd10b6ef01957077dac
RamiroFerradas <ramiferra97@gmail.com>
2023-06-16 22:44:10 -0300

    update detail product and route checkout

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,11 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 // SDK de Mercado Pago
 const mercadopago = require("mercadopago");
+
+const ACCES_TOKEN = process.env.NEXT_PUBLIC_MP_ACCES_TOKEN_TEST;
+// const ACCES_TOKEN = process.env.NEXT_PUBLIC_MP_ACCES_TOKEN //REAL;
 // Agrega credenciales
 mercadopago.configure({
-  access_token:
-    "APP_USR-2187884601335675-060822-f815befbcaaa62b54b00dd2a0c463fe9-1394988542",
-  // "APP_USR-5628114857087145-060712-a1f3a749ca6d267a1c2c5905c6bdea7f-169857175", //real
+  access_token: ACCES_TOKEN,
 });
 
 // Crear un objeto de preferencia
