d9a1d6719a1e7c34360d0d1aa59936d35b9ec10e
RamiroFerradas <ramiferra97@gmail.com>
2023-07-19 20:06:42 -0300

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ const myFont = localFont({
   src: "../public/fonts/BNChester.otf",
 });
 
-export const metadata = {
-  // title: "Jotaté Climatización",
-  // description: "Climatización Ecológica",
-};
+// export const metadata = {
+//   title: "Jotaté Climatización",
+//   description: "Climatización Ecológica",
+// };
 
 export type RootLayoutProps = {
   children: React.ReactNode;
@@ -23,6 +23,25 @@ export type RootLayoutProps = {
 const RootLayout: NextPage<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KNWJJVW');
+            `,
+          }}
+        />
+        <link rel="icon" href="./favicon.ico" />
+        <link
+          rel="canonical"
+          href="https://www.jotateclimatizacion.com/productos"
+        />
+        <title>My page</title>
+      </Head>
       <body className={inter.className}>
         {/* Agrega el código de Google Tag Manager después de la etiqueta <body> */}
         <noscript
--- app/redux/reduxProvider.tsx
+++ app/redux/reduxProvider.tsx
@@ -4,7 +4,6 @@ import { store } from "./store";
 import { NextPage } from "next";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@material-tailwind/react";
-import Head from "next/head";
 
 export type RootLayoutProps = {
   children: React.ReactNode;
@@ -13,25 +12,6 @@ export type RootLayoutProps = {
 const ReduxProvider: NextPage<RootLayoutProps> = ({ children }) => {
   return (
     <Provider store={store}>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KNWJJVW');
-            `,
-          }}
-        />
-        <link rel="icon" href="../favicon.ico" />
-        <link
-          rel="canonical"
-          href="https://www.jotateclimatizacion.com/productos"
-        />
-        <title>Jotaaaaaaaa</title>
-      </Head>
       <ThemeProvider>{children}</ThemeProvider>
     </Provider>
   );
