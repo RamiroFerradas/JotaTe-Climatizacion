4bc62bde48ae69e07bdb20c77af49f3fbfa3bc8c
Ramiro <ramiferra97@gmail.com>
2024-02-04 15:17:16 -0300

    analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import { NextPage } from "next";
 import { ReactNode } from "react";
 import "./tailwind.globals.css";
-import { Inter, Nunito, Roboto, Rubik } from "next/font/google";
-import Head from "next/head";
+import { Rubik } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import localFont from "next/font/local";
 import ReduxProvider from "./redux/reduxProvider";
-import { Footer } from "./components";
 const inter = Rubik({ subsets: ["latin"], weight: "300" });
 import Favicon from "../public/favicon.ico";
 
@@ -34,6 +33,7 @@ const RootLayout: NextPage<RootLayoutProps> = ({ children }) => {
           <div className="min-h-screen flex flex-col">
             <div className="flex-grow flex flex-col">{children}</div>
           </div>
+          <Analytics />
         </ReduxProvider>
       </body>
     </html>
