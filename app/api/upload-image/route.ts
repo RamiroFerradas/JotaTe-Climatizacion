6591c6feb0d9e188d120d0afe7c59be8392c3f85
Ramiro <ramiferra97@gmail.com>
2024-02-06 15:50:19 -0300

    fix images cors

--- app/api/upload-image/route.ts
+++ app/api/upload-image/route.ts
@@ -1,8 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 import { writeFile } from "fs/promises";
 import cloudinary from "@/app/lib/cloudinary";
+import { headers } from "next/headers";
 
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+export async function OPTIONS(req: NextRequest) {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
 export async function POST(request) {
   const enviroment = process.env.NEXT_PUBLIC_ENVIRONMENT;
 
@@ -22,6 +31,7 @@ export async function POST(request) {
     const bytes = await image.arrayBuffer();
     const buffer = Buffer.from(bytes);
     const filePath = path.join(process.cwd(), "public", image.name);
+
     await writeFile(filePath, buffer);
 
     const result = await cloudinary.uploader.upload(filePath, {
@@ -29,9 +39,15 @@ export async function POST(request) {
         enviroment === "dev" ? "DEV" : "JotaTe Climatizacion"
       }/${brand}/${category}/${subcategory}/${name}`,
     });
-    console.log(result);
+
+    // Crear una copia del objeto result y eliminar la propiedad api_key
     const resultCopy = { ...result };
     delete resultCopy.api_key;
+
+    // Agregar las headers de CORS
+    resultCopy.headers = corsHeaders;
+
+    // Devolver la copia adaptada del resultado sin la propiedad api_key
 
     return NextResponse.json(resultCopy);
   } catch (error) {
