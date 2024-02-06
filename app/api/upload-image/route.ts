c6a77f721ca5b1e4b581da6236d7dc52ac16919f
Ramiro <ramiferra97@gmail.com>
2024-02-06 18:07:55 -0300

    fix errores

--- app/api/upload-image/route.ts
+++ app/api/upload-image/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import path from "path";
 import { writeFile } from "fs/promises";
 import cloudinary from "@/app/lib/cloudinary";
-// import NextCors from "nextjs-cors";
 
 export async function POST(req, res) {
   const enviroment = process.env.NEXT_PUBLIC_ENVIRONMENT;
@@ -16,24 +15,13 @@ export async function POST(req, res) {
     const category = data.get("category");
     const subcategory = data.get("subcategory");
 
-    // res.headers.append("Access-Control-Allow-Credentials", "false");
-    // res.headers.append("Access-Control-Allow-Origin", "*");
-    // res.headers.append(
-    //   "Access-Control-Allow-Methods",
-    //   "GET,DELETE,PATCH,POST,PUT"
-    // );
-    // res.headers.append(
-    //   "Access-Control-Allow-Headers",
-    //   "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date"
-    // );
-
     if (!image) {
       return NextResponse.json("No se ha detectado una imagen para subir");
     }
 
     const bytes = await image.arrayBuffer();
     const buffer = Buffer.from(bytes);
-    const filePath = path.join(process.cwd(), "public", image.name);
+    const filePath = path.join("/tmp", image.name);
     await writeFile(filePath, buffer);
 
     const result = await cloudinary.uploader.upload(filePath, {
@@ -44,12 +32,7 @@ export async function POST(req, res) {
 
     const resultCopy = { ...result };
     delete resultCopy.api_key;
-    // await NextCors(req, res, {
-    //   // Options
-    //   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    //   origin: "*",
-    //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    // });
+
     console.log(res);
     return NextResponse.json(resultCopy);
   } catch (error) {
