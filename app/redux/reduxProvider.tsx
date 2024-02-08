a09294d3de9a9200c82afdb3e55745141805d3d1
Ramiro <ramiferra97@gmail.com>
2024-02-08 18:08:51 -0300

    fix update prices

--- app/redux/reduxProvider.tsx
+++ app/redux/reduxProvider.tsx
@@ -2,13 +2,13 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { NextPage } from "next";
-import { Toaster } from "react-hot-toast";
+
 import { ThemeProvider } from "@material-tailwind/react";
 import TagManager from "react-gtm-module";
-import { ReactNode, useEffect } from "react";
+import { useEffect } from "react";
 
 export type RootLayoutProps = {
-  children: any;
+  children: any
 };
 
 const ReduxProvider: NextPage<RootLayoutProps> = ({ children }) => {
