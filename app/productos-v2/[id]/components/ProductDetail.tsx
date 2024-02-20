927e70d6b169f49f157cfe27804760e1ca4188ae
Ramiro <ramiferra97@gmail.com>
2024-02-20 12:57:13 -0300

    fix

--- app/productos-v2/[id]/components/ProductDetail.tsx
+++ app/productos-v2/[id]/components/ProductDetail.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { parseCurrency } from "@/app/utilities/parseCurrency";
 import { toastAddToCart } from "@/app/utilities/toastAddToCart";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { FaCartPlus } from "react-icons/fa";
 import { BsWhatsapp } from "react-icons/bs";
 import Drawer from "@mui/material/Drawer";
@@ -18,8 +18,6 @@ import { Button } from "@material-tailwind/react";
 
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { formattedText } from "@/app/utilities/formattedText";
-import { Loading } from "@/app/components";
-import { useConditionProducts } from "@/app/hooks";
 import { useRouter, useSearchParams } from "next/navigation";
 import ImagesProduct from "./ImagesProduct";
 import { ProductCard } from "../../components";
@@ -27,6 +25,7 @@ import { CartProduct, Product } from "@/app/models";
 import { useCart } from "../../context/CartContext";
 import { updateConsults } from "@/app/services/crud/updateConsults";
 import Link from "next/link";
+import Loading from "../loading";
 
 type Props = {
   selectedProduct: Product;
@@ -107,136 +106,138 @@ const ProductDetail = ({ selectedProduct, recommendedProducts }: Props) => {
       className="min-h-screen z-50"
     >
       <Card className="flex flex-col md:w-[48rem] justify-between items-center px-0 h-full overflow-y-auto w-screen">
-        <div className="overflow-y-auto w-full">
-          {!selectedProduct ? (
-            <div className="w-[48vw] flex flex-col">
-              <Loading />
-            </div>
-          ) : (
-            <div className="flex flex-col min-h-screen justify-between">
-              <div>
-                <button
-                  onClick={backModal()}
-                  className="absolute left-4 md:left-1 top-0 z-50"
-                >
-                  <KeyboardBackspaceIcon fontSize="large" />
-                </button>
-                <CardMedia className="h-full w-full">
-                  <ImagesProduct
-                    zoom={zoom}
-                    setZoom={setZoom}
-                    selectedProduct={selectedProduct}
-                  />
-                </CardMedia>
+        <Suspense fallback={<Loading />}>
+          <div className="overflow-y-auto w-full">
+            {!selectedProduct ? (
+              <div className="w-[48vw] flex flex-col">
+                <Loading />
               </div>
-              <div className="px-3 md:px-8">
-                <div
-                  className={`p-2 transition-all w-full flex justify-center items-start flex-col gap-5 ${
-                    !zoom ? "block" : "md:hidden"
-                  }`}
-                >
-                  <div className="h-1/6">
-                    <Typography
-                      variant="h5"
-                      component="div"
-                      className="font-black uppercase"
-                    >
-                      {selectedProduct?.name}
-                    </Typography>
-                    <Typography
-                      gutterBottom
-                      variant="h6"
-                      component="div"
-                      className="text-gray-600"
-                    >
-                      {selectedProduct?.brand as string}
-                    </Typography>
+            ) : (
+              <div className="flex flex-col min-h-screen justify-between">
+                <div>
+                  <button
+                    onClick={backModal()}
+                    className="absolute left-4 md:left-1 top-0 z-50"
+                  >
+                    <KeyboardBackspaceIcon fontSize="large" />
+                  </button>
+                  <CardMedia className="h-full w-full">
+                    <ImagesProduct
+                      zoom={zoom}
+                      setZoom={setZoom}
+                      selectedProduct={selectedProduct}
+                    />
+                  </CardMedia>
+                </div>
+                <div className="px-3 md:px-8">
+                  <div
+                    className={`p-2 transition-all w-full flex justify-center items-start flex-col gap-5 ${
+                      !zoom ? "block" : "md:hidden"
+                    }`}
+                  >
+                    <div className="h-1/6">
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        className="font-black uppercase"
+                      >
+                        {selectedProduct?.name}
+                      </Typography>
+                      <Typography
+                        gutterBottom
+                        variant="h6"
+                        component="div"
+                        className="text-gray-600"
+                      >
+                        {selectedProduct?.brand as string}
+                      </Typography>
+                      <Divider />
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center flex-growh-auto w-full">
+                      <ul className="w-full overflow-y-auto">
+                        {formattedDescription?.map((linea, index) => (
+                          <li key={index} className="mb-1">
+                            <Typography variant="body2" color="text.secondary">
+                              {linea}
+                            </Typography>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                     <Divider />
                   </div>
-
-                  <div className="flex flex-col justify-center items-center flex-growh-auto w-full">
-                    <ul className="w-full overflow-y-auto">
-                      {formattedDescription?.map((linea, index) => (
-                        <li key={index} className="mb-1">
-                          <Typography variant="body2" color="text.secondary">
-                            {linea}
-                          </Typography>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <Divider />
-                </div>
-                {recommendedProducts.length ? (
-                  <div className={`w-full ${!zoom ? "block" : "md:hidden"}`}>
-                    <Typography
-                      variant="h6"
-                      component="div"
-                      className="font-semibold"
-                    >
-                      ¡Agrega tambien alguna de estas opciones!
-                    </Typography>
-                    <div className="flex flex-wrap gap-6 mt-2 justify-start items-center">
-                      {recommendedProducts.map((kit, index) => (
-                        <Link
-                          key={kit.id + index}
-                          shallow
-                          scroll={false}
-                          href={`/productos-v2/${kit.id}`}
-                        >
-                          <ProductCard product={kit} />
-                        </Link>
-                      ))}
+                  {recommendedProducts.length ? (
+                    <div className={`w-full ${!zoom ? "block" : "md:hidden"}`}>
+                      <Typography
+                        variant="h6"
+                        component="div"
+                        className="font-semibold"
+                      >
+                        ¡Agrega tambien alguna de estas opciones!
+                      </Typography>
+                      <div className="flex flex-wrap gap-6 mt-2 justify-start items-center">
+                        {recommendedProducts.map((kit, index) => (
+                          <Link
+                            key={kit.id + index}
+                            shallow
+                            scroll={false}
+                            href={`/productos-v2/${kit.id}`}
+                          >
+                            <ProductCard product={kit} />
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                  <div className={!zoom ? "block mt-10" : "hidden"}>
+                    <Divider />
+                    <div className="flex justify-between items-center text-xl font-bold w-full ">
+                      <Typography
+                        variant="h6"
+                        component="div"
+                        className="font-black "
+                      >
+                        Precio
+                      </Typography>
+                      <Typography
+                        variant="h6"
+                        component="div"
+                        className="font-black uppercase"
+                      >
+                        {parseCurrency(Number(selectedProduct?.price))}
+                      </Typography>
                     </div>
                   </div>
-                ) : (
-                  <></>
-                )}
-                <div className={!zoom ? "block mt-10" : "hidden"}>
-                  <Divider />
-                  <div className="flex justify-between items-center text-xl font-bold w-full ">
-                    <Typography
-                      variant="h6"
-                      component="div"
-                      className="font-black "
-                    >
-                      Precio
-                    </Typography>
-                    <Typography
-                      variant="h6"
-                      component="div"
-                      className="font-black uppercase"
-                    >
-                      {parseCurrency(Number(selectedProduct?.price))}
-                    </Typography>
-                  </div>
+                </div>
+                <div className={`sticky bottom-0 ${zoom ? "hidden" : ""}`}>
+                  <CardActions>
+                    <div className="w-full items-center flex justify-center gap-5">
+                      <Button
+                        className="flex w-full items-center gap-3 justify-center"
+                        onClick={handleAddToCart}
+                      >
+                        <FaCartPlus className="h-5 w-5" />
+                        <span className="hidden md:block">Agregar</span>
+                      </Button>
+
+                      <Button
+                        color="green"
+                        className="flex items-center gap-3 w-full justify-center"
+                        onClick={handleConsultProduct}
+                      >
+                        <BsWhatsapp className="h-5 w-5" />
+                        <span>Consultar</span>
+                      </Button>
+                    </div>
+                  </CardActions>
                 </div>
               </div>
-              <div className={`sticky bottom-0 ${zoom ? "hidden" : ""}`}>
-                <CardActions>
-                  <div className="w-full items-center flex justify-center gap-5">
-                    <Button
-                      className="flex w-full items-center gap-3 justify-center"
-                      onClick={handleAddToCart}
-                    >
-                      <FaCartPlus className="h-5 w-5" />
-                      <span className="hidden md:block">Agregar</span>
-                    </Button>
-
-                    <Button
-                      color="green"
-                      className="flex items-center gap-3 w-full justify-center"
-                      onClick={handleConsultProduct}
-                    >
-                      <BsWhatsapp className="h-5 w-5" />
-                      <span>Consultar</span>
-                    </Button>
-                  </div>
-                </CardActions>
-              </div>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        </Suspense>
       </Card>
     </Drawer>
   );
--- app/productos-v2/[id]/page.tsx
+++ app/productos-v2/[id]/page.tsx
@@ -1,19 +1,25 @@
+"use server";
 import { fetchProductById } from "@/app/services/fetchs/fetchProducts";
 import ProductDetail from "./components/ProductDetail";
 import { fetchRecommendedProductsByIds } from "@/app/services/fetchs/fetchRecommendedProductsByIds";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 type Props = {
-  params: any;
+  params: { id: string };
 };
+
 export default async function ProductPage({ params }: Props) {
   const selectedProduct = await fetchProductById(params.id);
   const recommendedProducts = await fetchRecommendedProductsByIds(
     selectedProduct.recommended
   );
   return (
-    <ProductDetail
-      selectedProduct={selectedProduct}
-      recommendedProducts={recommendedProducts}
-    />
+    <Suspense key={selectedProduct.id} fallback={<Loading />}>
+      <ProductDetail
+        selectedProduct={selectedProduct}
+        recommendedProducts={recommendedProducts}
+      />
+    </Suspense>
   );
 }
