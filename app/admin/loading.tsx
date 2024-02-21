21a4aa59cf0c0886f4030cf41a76b5c9ce42c9fd
Ramiro <ramiferra97@gmail.com>
2024-02-21 11:51:16 -0300

    loading admin

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -1,58 +1,10 @@
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Divider,
-  Drawer,
-  Skeleton,
-} from "@mui/material";
+import { CircularProgress } from "@mui/material";
 
 type Props = {};
-export default function Loading({}: Props) {
+export default function LoadingProducts({}: Props) {
   return (
-    <div className="overflow-y-auto w-full">
-      <div className="flex flex-col min-h-screen justify-between">
-        <div>
-          <CardMedia className="h-full w-full">
-            <Skeleton variant="rectangular" width={"100%"} height={210} />
-          </CardMedia>
-          <div className="px-3 md:px-8">
-            <div className="p-2 transition-all w-full flex justify-center items-start flex-col gap-5">
-              <Skeleton width={200} height={25} animation="wave" />
-              <Skeleton width={140} height={20} animation="wave" />
-              <Skeleton width="60%" />
-              <Skeleton width="60%" />
-              <Skeleton width="60%" />
-              <Skeleton width="70%" />
-              <Skeleton width="70%" />
-              <Skeleton width="60%" />
-
-              <Divider />
-            </div>
-          </div>
-        </div>
-
-        <div className="sticky bottom-0">
-          <CardContent>
-            <div className="w-full items-center flex justify-between gap-5">
-              <Skeleton
-                variant="rectangular"
-                width={"45%"}
-                height={40}
-                animation="wave"
-                className="rounded-lg"
-              />
-              <Skeleton
-                variant="rectangular"
-                width={"50%"}
-                height={40}
-                animation="wave"
-                className="rounded-lg"
-              />
-            </div>
-          </CardContent>
-        </div>
-      </div>
+    <div className=" h-screen w-screen">
+      <CircularProgress size={45} color="success" />
     </div>
   );
 }
