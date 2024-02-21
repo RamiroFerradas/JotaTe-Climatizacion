2b94ea35ce09fcd97facc547e46ae8477f1b33e5
Ramiro <ramiferra97@gmail.com>
2024-02-21 11:38:59 -0300

    fix

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from "./index";
-export { default as useConditionProducts } from "./useConditionProducts";
 export { default as useLocalStorage } from "./useLocalStorage";
 export { default as useScreenSize } from "./useScreenSize";
 export { default as useScrollSections } from "./useScrollSections";
