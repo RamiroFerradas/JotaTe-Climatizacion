19c342739cb4a35fa64386a21aa4b89b51e8c3e4
Ramiro <ramiferra97@gmail.com>
2024-02-21 11:52:16 -0300

    loading admin

--- app/admin/loading.tsx
+++ app/admin/loading.tsx
@@ -3,7 +3,7 @@ import { CircularProgress } from "@mui/material";
 type Props = {};
 export default function LoadingProducts({}: Props) {
   return (
-    <div className=" h-screen w-screen">
+    <div className=" h-screen w-screen flex justify-center items-center">
       <CircularProgress size={45} color="success" />
     </div>
   );
