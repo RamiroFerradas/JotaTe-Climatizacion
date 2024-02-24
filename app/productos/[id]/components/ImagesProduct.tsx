a16a94e8f5ebc27e54c4336b96a815ceeba7ff41
Ramiro <ramiferra97@gmail.com>
2024-02-24 15:25:43 -0300

    fix images

--- app/productos/[id]/components/ImagesProduct.tsx
+++ app/productos/[id]/components/ImagesProduct.tsx
@@ -88,7 +88,6 @@ const ImagesProduct: React.FC<ImagesProductProps> = ({
               height={!zoom ? 200 : 320}
               src={img}
               alt={selectedProduct.name}
-              onLoad={() => <Loading />}
               className={`md:max-w-3xl max-w-screen pb-20 object-cover md:transition-all ${
                 !zoom
                   ? "md:h-80 hover:cursor-zoom-in pointer-events-none md:pointer-events-auto"
