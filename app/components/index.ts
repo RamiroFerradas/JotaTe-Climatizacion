a82a57b00a1c34feedbb88ed5f7b0187898940a9
Ramiro <ramiferra97@gmail.com>
2024-02-24 01:48:13 -0300

    seo

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,5 @@
 export * from "./Footer";
+export { default as InputForm } from "./InputForm";
 export * from "./Loading";
+export { default as Seo } from "./Seo";
 export * from "./index";
--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -9,6 +9,8 @@ import LoadingProducts from "../loading";
 import { Card, Drawer } from "@mui/material";
 import LoadingProduct from "./loading";
 import Head from "next/head";
+import { Seo } from "@/app/components";
+import { title } from "process";
 
 type Props = {
   params: { id: string };
@@ -23,19 +25,12 @@ export default async function ProductPage({ params }: Props) {
 
   return (
     <>
-      <Head>
-        <meta property="og:title" content={selectedProduct?.name} />
-        <meta
-          property="og:description"
-          content={selectedProduct?.description}
-        />
-        <meta property="og:image" content={selectedProduct?.image[0]} />
-        <meta
-          property="og:url"
-          content={`https://www.jotateclimatizacion.com/productos/${selectedProduct?.id}`}
-        />
-        <meta property="og:type" content="product" />
-      </Head>
+      <Seo
+        id={selectedProduct.id}
+        description={selectedProduct.name}
+        img={selectedProduct.image[0]}
+        title={selectedProduct.name}
+      />
       <ProductDetail
         selectedProduct={selectedProduct}
         recommendedProducts={recommendedProducts}
