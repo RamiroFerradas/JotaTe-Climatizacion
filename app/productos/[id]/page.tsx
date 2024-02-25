94ee34a96ecdede4df9e0c2b862ec2e1e72311d8
Ramiro <ramiferra97@gmail.com>
2024-02-25 13:12:52 -0300

    metadata

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -3,31 +3,50 @@ import ProductDetail from "./components/ProductDetail";
 import { fetchRecommendedProductsByIds } from "@/app/services/fetchs/fetchRecommendedProductsByIds";
 import { Seo } from "@/app/components";
 import { redirect } from "next/navigation";
+import { Metadata, ResolvingMetadata } from "next";
+import { Product } from "@/app/models";
 
 type Props = {
   params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+  product: Product;
 };
 
-export default async function ProductPage({ params }: Props) {
-  const selectedProduct = await fetchProductById(params.id);
-  const recommendedProductIds = selectedProduct?.recommended?.products;
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.id;
+
+  const product = await fetchProductById(id);
+  const previousImages = (await parent).openGraph?.images || [];
+  const productImage = product?.image || "/default-product-image.jpg"; // Adjust the default image accordingly
+  console.log(previousImages);
+
+  return {
+    title: product.name,
+    openGraph: {
+      images: [productImage, ...previousImages],
+    },
+  };
+}
+
+export default async function Page({ params, searchParams }: Props) {
+  const product = await fetchProductById(params.id);
+  const recommendedProductIds = product?.recommended?.products;
+  console.log(product);
+
+  if (!product) {
+    return redirect("/productos");
+  }
   const recommendedProducts =
     recommendedProductIds &&
     (await fetchRecommendedProductsByIds(recommendedProductIds));
 
-  if (!selectedProduct) {
-    return redirect("/productos");
-  }
   return (
     <>
-      <Seo
-        id={selectedProduct?.id}
-        description={selectedProduct?.name}
-        img={selectedProduct?.image[0]}
-        title={selectedProduct?.name}
-      />
       <ProductDetail
-        selectedProduct={selectedProduct}
+        selectedProduct={product}
         recommendedProducts={recommendedProducts}
       />
     </>
